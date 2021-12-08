dc3c34ce95a9bf9fa1806878e19970c7db40184d
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2021-12-08 09:51:42 -0300

    Finalizado o Componente NavBar
    Finalizado o Componente NavBar

--- frontend/src/pages/Dashboard/components/NavBarComponent.vue
+++ frontend/src/pages/Dashboard/components/NavBarComponent.vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
+  <div class="navbar">
     <h3 class="logo"><i class="fa fa-user"></i> Logo</h3>
+    <a @click.prevent="logout" class="logout"><i class="fa fa-sign-out-alt"></i> Sair</a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'NavBarComponent',
+  methods: {
+    logout() {
+      this.$router.push({name: 'Login'})
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+.navbar{
+
   .logo {
-    padding-top:5px;
-    padding-left: 10px;
-    color: white;
+      padding-top:5px;
+      padding-left: 10px;
+      color: white;
+    }
+
+  .logout {
+      color: white;
+      text-decoration: none;
+      font-size: 1.2rem;
   }
+
+  .logout:hover {
+    color: rgb(202, 202, 212);
+  }
+
+ }
+ 
 </style>
