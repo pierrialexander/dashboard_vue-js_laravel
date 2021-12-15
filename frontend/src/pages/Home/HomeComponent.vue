18e78570654c4e46315f85672762125fb230042b
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2021-12-15 14:13:13 -0300

    Commit
    Commit

--- frontend/src/pages/Home/HomeComponent.vue
+++ frontend/src/pages/Home/HomeComponent.vue
@@ -19,9 +19,28 @@
               <CardsComponent :type="'Vendas'" :porcentagem="'12%'" :icon="'fa-chart-line'" :qtd="'3270'"/>
             </div>
             <div class="col-12 col-lg-3">
-              <CardsComponent :type="'Serviços'" :porcentagem="'9%'" :icon="'fa-users'" :qtd="'4120'"/>
+              <CardsComponent :type="'Serviços'" :porcentagem="'9%'" :icon="'fa-cogs'" :qtd="'4120'"/>
             </div>
-             
+                         
+          </div>
+
+          <br>
+
+          <div class="row">
+          
+            <div class="col-12 col-lg-3">
+              <CardsComponent :type="'Clientes'" :porcentagem="'7%'" :icon="'fa-users'" :qtd="'5590'"/>
+            </div>
+            <div class="col-12 col-lg-3">
+              <CardsComponent :type="'Produtos'" :porcentagem="'5%'" :icon="'fa-dolly-flatbed'" :qtd="'15800'"/>
+            </div>
+            <div class="col-12 col-lg-3">
+              <CardsComponent :type="'Vendas'" :porcentagem="'12%'" :icon="'fa-chart-line'" :qtd="'3270'"/>
+            </div>
+            <div class="col-12 col-lg-3">
+              <CardsComponent :type="'Serviços'" :porcentagem="'9%'" :icon="'fa-cogs'" :qtd="'4120'"/>
+            </div>
+                         
           </div>
           
         </div>
